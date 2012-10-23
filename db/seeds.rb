30a9f674449a0e81872fea5ec57a887c98223bee
Benjamin <github@dev.telepath.de>
2012-10-23 22:20:39 +0200

    install page_images and search extract refinerycms-projector extension to own repository

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ Refinery::Pages::Engine.load_seed
 
 # Added by Refinery CMS Projectors extension
 Refinery::Projectors::Engine.load_seed
+
+# Added by Refinery CMS Search engine
+Refinery::Search::Engine.load_seed
--- vendor/extensions/projectors/spec/support/factories/refinery/projectors.rb
+++
@@ -1,7 +0,0 @@
-
-FactoryGirl.define do
-  factory :projector, :class => Refinery::Projectors::Projector do
-    sequence(:name) { |n| "refinery#{n}" }
-  end
-end
-
